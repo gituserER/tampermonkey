29be43527fbab9a67778846c5a9f8304f3e57e3b
gituserER <83885089+gituserER@users.noreply.github.com>
2025-01-18 13:46:29 +0700

    Update replacing_links.js

--- igruha/replacing_links.js
+++ igruha/replacing_links.js
@@ -1,3 +1,4 @@
+if (!/https:\/\/itorrents-igruha\.org\/.*\.html$/.test(location.href)) return;
 document.querySelectorAll('a[href*="/?do=download&id="]').forEach(link => {
       link.href = link.href.replace('/?do=download&id=', '/engine/download.php?id=');
   });
